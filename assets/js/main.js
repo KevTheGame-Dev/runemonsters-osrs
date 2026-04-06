3c4a51c418ec022403b16b8517e6347f909cf841
Kevin <kevincb45@gmail.com>
2026-04-06 19:56:55 -0400

    fix submenus in rulebook nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,4 +182,25 @@
 
 			});
 
+	// Menu.
+	$('.toggleButton').each(function (index, element) {
+		var targetId = $(this).attr('targetId');
+		var target = $('#' + targetId);
+
+		if (target.attr('start') == 'closed') {
+			target.toggle();
+		}
+
+		$(this).on('click', function(event) {
+			target.toggle();
+
+			if ($(this).text() == '▶') {
+				$(this).html('▼');
+			} else {
+				$(this).html('▶');
+			}
+			event.stopPropagation();
+		});
+	})
+	
 })(jQuery);
